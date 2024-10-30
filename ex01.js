1fe8c820503ef4fda118fdb3d1c62d8941e242ab
LuisGSCunha <luis.gustavo.scunha05@gmail.com>
2024-10-30 19:25:10 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,10 @@
+//amarzena o numero
+let num = 16
+// calcule o resultado
+let resultado = (num % 2)
+// diferencie de impar ou par
+if(resultado == 0){
+    console.log("O Resultado é Par")
+}else{
+    console.log("O Resultado é Impar")
+}
---
+++ ex02.js
@@ -0,0 +1,20 @@
+//armazenar a nota do aluno
+
+let nota1 = 5
+
+let nota2 =  2
+
+//CALCULA A MEDIA
+
+let media = (nota1 + nota2)/2
+
+
+
+
+if(media >= 6){
+    console.log("Aluno Aprovado!")
+}else if(media < 4){
+    console.log("Aluno Reprovado!")
+}else{
+    console.log("Aluno em Recuperação!")
+}
